08eb57a0bbf209b82aa58a190dbe2b519689fa68
bartonjoe <bartonjoe@bartonjoe-VirtualBox>
2017-07-31 17:49:34 +0800

    alpha filesystem

--- mFileSystem/index.php
+++ mFileSystem/index.php
@@ -90,12 +90,67 @@ $router->add('/get_file', function() {
             $fileDir = $behavir->queryFile($fileDir, $fileName);
             $fileName = $linkRecord[0]['open_id'] . '.png';
         }
-        Header("Content-type: " . $fileType);
-        Header("Accept-Ranges: bytes");
-        Header("Accept-Length: " . filesize(__FILEROOT__ . __OPENDIR__ . $fileDir . $fileName));
-        Header("Content-Transfer-Encoding: binary");
-        readfile(__FILEROOT__ . __OPENDIR__ . $fileDir . $fileName);
-        exit();
+
+        $size = filesize(__FILEROOT__ . __OPENDIR__ . $fileDir . $fileName); // File size
+        header("Content-type: $fileType");
+
+        if (isset($_SERVER['HTTP_RANGE'])) {
+            $fp = @fopen(__FILEROOT__ . __OPENDIR__ . $fileDir . $fileName, 'rb');
+
+            $length = $size;
+            $start = 0;
+            $end = $size - 1;
+            header("Accept-Ranges: 0-$length");
+            if (isset($_SERVER['HTTP_RANGE'])) {
+
+                $c_start = $start;
+                $c_end = $end;
+                list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+                if (strpos($range, ',') !== false) {
+                    header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                    header("Content-Range: bytes $start-$end/$size");
+                    exit;
+                }
+                if ($range0 == '-') {
+                    $c_start = $size - substr($range, 1);
+                } else {
+
+                    $range = explode('-', $range);
+                    $c_start = $range[0];
+                    $c_end = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+                }
+                $c_end = ($c_end > $end) ? $end : $c_end;
+                if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+
+                    header('HTTP/1.1 416 Requested Range Not Satisfiable');
+                    header("Content-Range: bytes $start-$end/$size");
+                    exit;
+                }
+                $start = $c_start;
+                $end = $c_end;
+                $length = $end - $start + 1;
+                fseek($fp, $start);
+                header('HTTP/1.1 206 Partial Content');
+            }
+            header("Content-Range: bytes $start-$end/$size");
+            header("Content-Length: $length");
+
+            $buffer = 1024 * 8;
+            while (!feof($fp) && ($p = ftell($fp)) <= $end) {
+
+                if ($p + $buffer > $end) {
+                    $buffer = $end - $p + 1;
+                }
+                set_time_limit(0);
+                echo fread($fp, $buffer);
+                flush();
+            }
+
+            fclose($fp);
+        } else {
+            header("Content-Length: $size");
+            readfile(__FILEROOT__ . __OPENDIR__ . $fileDir . $fileName);
+        }
     } else {
         if (!file_exists(__FILEROOT__ . __ARCHIVEDIR__ . $fileDir . $fileName)) {
             $result->ReturnCode = '10014';
@@ -111,7 +166,7 @@ $router->add('/get_file', function() {
         if (!is_null($linkRecord[0]['file_name']) && !empty($linkRecord[0]['file_name'])) {
             $dlFileName = $linkRecord[0]['file_name'];
         } else {
-            $dlFileName = $file;
+            $dlFileName = $fileName;
         }
 
         Header("Content-Disposition: attachment; filename=" . $dlFileName);
